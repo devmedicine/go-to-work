3728403916d79e704f05b4528492e049f49ab10e
Daikichi <daikiti412@gmail.com>
2020-04-28 14:56:03 +0900

    add charts/bar view.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 // requires
 const moment = require('moment');
+moment.locale('ja');
 const fs = require('fs')
 const express = require("express");
 const sqlite3 = require('sqlite3').verbose();
@@ -10,20 +11,30 @@ const bodyParser = require('body-parser');
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const LUNCH_BREAK = 1;
 
 // express settings
 const app = express();
 app.use(bodyParser.json())
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 // DB初期化
 if (!fs.existsSync('punchData')) {
     const initDb = new sqlite3.Database('punchData');
     initDb.serialize(function () {
-        initDb.run('CREATE TABLE punch (user_id TEXT, punch_date_time TEXT, is_in integer)');
+        initDb.run('CREATE TABLE punch (user_id TEXT, punch_date TEXT, punch_time TEXT, is_in integer, primary key(user_id, punch_date, is_in))');
     });
     initDb.close();
 }
 
+app.get('/', function (req, response) {
+    response.render('index', {});
+})
+
+/**
+ * 記録データ全て出力
+ */
 app.get("/show/all", function (req, res) {
     const db = new sqlite3.Database('punchData');
     db.serialize(function () {
@@ -34,12 +45,17 @@ app.get("/show/all", function (req, res) {
     db.close();
 });
 
-app.get("/show/:userId", function (req, res) {
+/**
+ * 勤務時間描画(棒グラフ)
+ */
+app.get("/charts/bar/:userId", function (req, res) {
     const db = new sqlite3.Database('punchData');
     db.serialize(function () {
-        const query = `SELECT * FROM punch WHERE user_id = ${req.params}`
+        const query = `SELECT * FROM punch WHERE user_id = '${req.params.userId}'`
         db.all(query, (err, rows) => {
-            res.json(rows)
+            const days = getDaysInThisMonth();
+            const retouchedRows = retouchPunchDataToBar(days, rows);
+            res.render('bar', { days, rows: retouchedRows, userId: req.params.userId });
         })
     });
     db.close();
@@ -56,8 +72,9 @@ app.post("/in", (req, res) => {
 /**
  * 退勤
  */
-app.post("/out", function (request, respond) {
+app.post("/out", function (req, res) {
     punching(1, req.body.user_id);
+    res.send('success!');
 });
 
 /**
@@ -67,15 +84,82 @@ app.post("/out", function (request, respond) {
  */
 const punching = (isIn, userId) => {
     const db = new sqlite3.Database('punchData');
-    const now = new Date().toFormat("YYYY/MM/DD HH24:MI:SS")
+    const nowDate = moment().format("YYYY/MM/DD");
+    const nowTime = moment().format("HH:mm:ss");
     // insert
     db.serialize(function () {
-        const stmt = db.prepare('INSERT INTO punch VALUES (?, ?, ?)');
-        stmt.run(userId, now, isIn)
+        const stmt = db.prepare('INSERT INTO punch VALUES (?, ?, ?, ?) ON CONFLICT(user_id, punch_date, is_in) do UPDATE SET punch_time = ?');
+        stmt.run(userId, nowDate, nowTime, isIn, nowTime)
         stmt.finalize();
     });
 
     db.close();
+}
+
+/**
+ * 出退勤時間差の配列を返す
+ * @param {*} days 
+ * @param {*} rows 
+ */
+const retouchPunchDataToBar = (days, rows) => {
+    let result = [];
+    if (rows.length <= 0) {
+        return result;
+    }
+
+    days.forEach(day => {
+        const sameDays = rows.filter(row => row.punch_date === day);
+        // 退勤まで登録されている日付
+        if (sameDays && sameDays[1]) {
+            const diff = hoursDiff(sameDays, true);
+            result.push(diff);
+        } else {
+            result.push(0);
+        }
+    });
+
+    return result;
+}
+
+    if (rows.length <= 0) {
+    return result;
+}
+
+
+/**
+ * 時間差を求める
+ * @param {object} records punchテーブルデータ
+ * @param {boolean} isMinusLunchBreak 昼休憩をマイナスするか
+ */
+const hoursDiff = (records, isMinusLunchBreak) => {
+    const format = 'YYYY-MM-DD HH:mm:ss';
+    const s = records[0].is_in == 0 ? `${records[0].punch_date} ${records[0].punch_time}` : `${records[1].punch_date} ${records[1].punch_time}`;
+    const e = records[0].is_in == 1 ? `${records[0].punch_date} ${records[0].punch_time}` : `${records[1].punch_date} ${records[1].punch_time}`;
+    const d = moment(e, format).diff(moment(s, format)) / (60 * 60 * 1000);
+
+    if(isMinusLunchBreak){
+        const diff = d > 4 ? d - LUNCH_BREAK : d;
+        return diff;
+    }
+
+    return d;
+}
+
+/**
+ * 当月の日付を配列にして返す
+ * @return {Array<string>} 当月日付の配列
+ */
+const getDaysInThisMonth = () => {
+    let result = [];
+    const s = moment().startOf('month');
+    const e = moment().endOf('month');
+
+    while (s.unix() <= e.unix()) {
+        result.push(s.format('YYYY/MM/DD'));
+        s.add(1, 'days');
+    }
+
+    return result;
 }
 
 //listenで待ち受け状態にする
